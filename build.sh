326ef632c7735a006ebe1378312816ff2839f407
bernsteining <bernsteining@protonmail.com>
2020-09-21 15:45:53 +0200

    Optimize binary size with wasm-opt

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ cargo build --target wasm32-unknown-unknown --release
 # bind it, with wasm-bindgen CLI (available here https://crates.io/crates/wasm-bindgen-cli).
 # be careful, wasm-bindgen CLI & wasm-bindgen versions have to match, you may have to
 # update wasm-bindgen to make it work
-wasm-bindgen ./target/wasm32-unknown-unknown/release/wkrakmi.wasm --out-dir build --no-typescript --no-modules
+wasm-bindgen ./target/wasm32-unknown-unknown/release/wkrakmi.wasm --out-dir build --no-typescript --no-modules --remove-name-section --remove-producers-section 
 
-# strip it with wasm-strip (from wabt: https://github.com/WebAssembly/wabt)
-wasm-strip build/wkrakmi_bg.wasm
+# optimize its size with wasm-opt from binaryen https://github.com/WebAssembly/binaryen
+wasm-opt -Oz -o build/wkrakmi_bg.wasm build/wkrakmi_bg.wasm
 
 # launch http serv
 basic-http-server build/
